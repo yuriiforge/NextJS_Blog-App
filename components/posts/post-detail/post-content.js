e17ff668da451670a0579e43509ce3daa98ef5f1
unknown <yurii.stepaniuk.dev@gmail.com>
2025-08-26 17:23:39 +0300

    Add react-markdown package

--- components/posts/post-detail/post-content.js
+++ components/posts/post-detail/post-content.js
@@ -1,5 +1,6 @@
 import PostHeader from './post-header';
 import classes from './post-content.module.css';
+import ReactMarkdown from 'react-markdown';
 
 const DUMMY_POST = {
   title: 'getting-started-with-nextjs',
@@ -15,7 +16,7 @@ const PostContent = () => {
   return (
     <article className={classes.content}>
       <PostHeader title={DUMMY_POST.title} image={imagePath} />
-      {DUMMY_POST.content}
+      <ReactMarkdown>{DUMMY_POST.content}</ReactMarkdown>
     </article>
   );
 };
