2cd1b40d5ec996bf4c2996cc31250c93dfdb54e7
unknown <yurii.stepaniuk.dev@gmail.com>
2025-08-27 01:12:21 +0300

    Add _document.js

---
+++ pages/_document.js
@@ -0,0 +1,18 @@
+import Document, { Html, Main, NextScript } from 'next/document';
+import Head from 'next/head';
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="en">
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
