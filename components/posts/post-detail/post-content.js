589b59a8f5f866c61c88f8a8a6f424ea258b567d
unknown <yurii.stepaniuk.dev@gmail.com>
2025-08-26 23:44:45 +0300

    Render markdown

--- components/posts/post-detail/post-content.js
+++ components/posts/post-detail/post-content.js
@@ -1,15 +1,40 @@
 import PostHeader from './post-header';
 import classes from './post-content.module.css';
 import ReactMarkdown from 'react-markdown';
+import Image from 'next/image';
 
 const PostContent = (props) => {
   const { post } = props;
   const imagePath = `/images/posts/${post.slug}/${post.image}.png`;
 
+  const customRendereres = {
+    paragraph(paragraph) {
+      const { node } = paragraph;
+
+      if (node.children[0].type === 'image') {
+        const image = node.children[0];
+
+        return (
+          <div className={classes.image}>
+            <Image
+              className={classes.fixedImage}
+              src={image.src}
+              alt={image.alt}
+            />
+          </div>
+        );
+      }
+
+      return <p>{paragraph.children}</p>;
+    },
+  };
+
   return (
     <article className={classes.content}>
       <PostHeader title={post.title} image={imagePath} />
-      <ReactMarkdown>{post.content}</ReactMarkdown>
+      <ReactMarkdown components={customRendereres}>
+        {post.content}
+      </ReactMarkdown>
     </article>
   );
 };
