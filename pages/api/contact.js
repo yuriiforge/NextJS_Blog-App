c213784903ccd76fcceeb9aae153b5a0d48eb43f
unknown <yurii.stepaniuk.dev@gmail.com>
2025-08-27 00:53:00 +0300

    Save feedback into DB

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -1,4 +1,6 @@
-function handler(req, res) {
+import clientPromise from '../../lib/mongodb';
+
+async function handler(req, res) {
   if (req.method === 'POST') {
     const { email, name, message } = req.body;
 
@@ -18,9 +20,26 @@ function handler(req, res) {
       email,
       name,
       message,
+      createdAt: new Date(),
     };
 
-    res.status(201).json({ message: 'Successfully stored message!' });
+    try {
+      const client = await clientPromise;
+      const db = client.db();
+
+      const result = await db.collection('messages').insertOne(newMessage);
+
+      res.status(201).json({
+        message: 'Successfully stored message!',
+        id: result.insertedId,
+      });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ message: 'Storing message failed!' });
+    }
+  } else {
+    res.setHeader('Allow', ['POST']);
+    res.status(405).end(`Method ${req.method} Not Allowed`);
   }
 }
 
